8e38c6e8d27e0bb6253aca193814a5ffc037a716
Alex Maldonado <66364639+AlexMNet@users.noreply.github.com>
2022-09-18 18:35:04 -0700

    btn-animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log('connected');
+function createRipple(event) {
+  const button = event.currentTarget;
+
+  // Calculate the dimensions of the circular span
+  const circle = document.createElement('span');
+  const diameter = Math.max(button.clientWidth, button.clientHeight);
+  const radius = diameter / 2;
+
+  circle.style.width = circle.style.height = `${diameter}px`;
+  circle.style.left = `${event.offsetX - radius}px`;
+  circle.style.top = `${event.offsetY - radius}px`;
+  circle.classList.add('ripple');
+
+  const ripple = button.getElementsByClassName('ripple')[0];
+
+  if (ripple) {
+    ripple.remove();
+  }
+
+  button.appendChild(circle);
+}
+
+const buttons = document.getElementsByTagName('button');
+for (const button of buttons) {
+  button.addEventListener('click', createRipple);
+}
